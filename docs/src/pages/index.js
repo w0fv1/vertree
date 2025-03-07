6e0a7f5978107f6ac0528a43b299143542a57a2d
W0FV1 <wofbi1@outlook.com>
2025-03-08 07:58:18 +0800

    优化首页图片展示

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -8,25 +8,30 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            快速开始 _>
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className={styles.heroContainer}>
+                {/* 左侧内容 */}
+                <div className={styles.heroText}>
+                    <Heading as="h1" className="hero__title">
+                        {siteConfig.title}
+                    </Heading>
+                    <p className="hero__subtitle">{siteConfig.tagline}</p>
+                    <div className={styles.buttons}>
+                        <Link className="button button--secondary button--lg" to="/docs/intro">
+                            快速开始 _>
+                        </Link>
+                    </div>
+                </div>
+
+                {/* 右侧图片 */}
+                <img src="/vertree/img/vertree_brand.png" alt="Logo" className={styles.heroImage} />
+            </div>
+        </header>
+    );
 }
+
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
