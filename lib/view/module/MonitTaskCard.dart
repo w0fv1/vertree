80440c67cb22d346bea74bd0ef158c70fe9b56dc
W0FV1 <wofbi1@outlook.com>
2025-04-03 16:55:42 +0800

    增加筛选

--- lib/view/module/MonitTaskCard.dart
+++ lib/view/module/MonitTaskCard.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:vertree/I18nLang.dart';
 import 'package:vertree/MonitService.dart';
 import 'package:vertree/component/FileUtils.dart';
 import 'package:vertree/component/Notifier.dart';
 import 'package:vertree/main.dart';
-
 
 class MonitTaskCard extends StatefulWidget {
   final FileMonitTask task;
@@ -47,6 +48,50 @@ class _MonitTaskCardState extends State<MonitTaskCard> {
     }
   }
 
+  void _cleanBackupFolder() {
+    if (task.backupDirPath != null) {
+      showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(appLocale.getText(AppLocale.monitcard_cleanDialogTitle)),
+            content: Text(appLocale.getText(AppLocale.monitcard_cleanDialogContent).tr([task.backupDirPath!])),
+            actions: <Widget>[
+              TextButton(
+                child: Text(appLocale.getText(AppLocale.monitcard_cleanDialogCancel)),
+                onPressed: () {
+                  Navigator.of(context).pop(false); // 返回 false，表示取消
+                },
+              ),
+              TextButton(
+                child: Text(appLocale.getText(AppLocale.monitcard_cleanDialogConfirm)),
+                onPressed: () {
+                  Navigator.of(context).pop(true); // 返回 true，表示确认
+                },
+              ),
+            ],
+          );
+        },
+      ).then((confirmed) {
+        if (confirmed != null && confirmed) {
+          try {
+            final directory = Directory(task.backupDirPath!);
+            final files = directory.listSync();
+
+            for (final file in files) {
+              if (file is File) {
+                file.deleteSync();
+              }
+            }
+            showToast(appLocale.getText(AppLocale.monitcard_cleanSuccess).tr([task.backupDirPath!]));
+          } catch (e) {
+            showToast(appLocale.getText(AppLocale.monitcard_cleanFail).tr([task.backupDirPath!, e.toString()]));
+            logger.error('删除备份文件夹中的文件时发生错误: $e');
+          }
+        }
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -54,33 +99,52 @@ class _MonitTaskCardState extends State<MonitTaskCard> {
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        child: Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(task.filePath, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                  const SizedBox(height: 4),
-                  if (task.backupDirPath != null)
-                    Text(
-                      appLocale.getText(AppLocale.monitcard_backupFolder).tr([task.backupDirPath!]),
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                ],
+            Text(task.filePath, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+            const SizedBox(height: 4),
+            if (task.backupDirPath != null)
+              Text(
+                appLocale.getText(AppLocale.monitcard_backupFolder).tr([task.backupDirPath!]),
+                style: Theme.of(context).textTheme.titleSmall,
               ),
-            ),
             Row(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Transform.scale(scale: 0.6, child: Switch(value: task.isRunning, onChanged: (_) => _toggleTask())),
-                IconButton(
-                  onPressed: () {
-                    widget.removeTask(task);
-                  },
-                  icon: const Icon(Icons.delete_outline_rounded),
+                Spacer(),
+                Tooltip(
+                  message: appLocale.getText(AppLocale.monitcard_pause),
+                  child: Transform.scale(
+                    scale: 0.7,
+                    child: Switch(value: task.isRunning, onChanged: (_) => _toggleTask()),
+                  ),
                 ),
-                IconButton(onPressed: _openBackupFolder, icon: const Icon(Icons.open_in_new_rounded, size: 22)),
+                Tooltip(
+                  message: appLocale.getText(AppLocale.monitcard_delete),
+                  child: IconButton(
+                    onPressed: () {
+                      widget.removeTask(task);
+                    },
+                    icon: const Icon(Icons.delete_outline_rounded),
+                  ),
+                ),
+                Tooltip(
+                  message: appLocale.getText(AppLocale.monitcard_clean),
+                  child: IconButton(
+                    onPressed: _cleanBackupFolder,
+                    icon: const Icon(Icons.cleaning_services_rounded, size: 22),
+                  ),
+                ),
+                Tooltip(
+                  message: appLocale.getText(AppLocale.monitcard_openBackupFolder),
+                  child: IconButton(
+                    onPressed: _openBackupFolder,
+                    icon: const Icon(Icons.open_in_new_rounded, size: 22),
+                  ),
+                ),
+
               ],
             ),
           ],
